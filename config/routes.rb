8d80fbf0ec33e750d1f49132cae8b88cea8d09a3
Daniel Santiago MJ <dsantiagomj@gmail.com>
2018-07-03 18:54:27 -0500

    Add username to Posts and link  to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
-
 Rails.application.routes.draw do
   root 'posts#index'
-  get 'posts', to: 'posts#index'
-  get 'posts/new'
-  post 'posts', to: 'posts#create'
-  get 'posts/:id', to: 'posts#show'
+
+  get  'posts',     to: 'posts#index'
+  get  'posts/new'
+  post 'posts',     to: 'posts#create'
+  get  'posts/:id', to: 'posts#show', as: 'posts_show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
